10768666373a031272955e15fef788493de59982
sespinaItba <sespina@itba.edu.ar>
2020-06-01 01:29:19 -0300

    quiero pullear nomas}2

--- Kernel/printManager.c
+++ Kernel/printManager.c
@@ -1,16 +1,18 @@
-void screenWriter(uint64_t buffer, uint64_t count, uint64_t upordown){
-	for (int i = 0; i < count; i++){
-		if(buffer[i]=='\n'){
-			newLine(upordown);
-		}
-		if(buffer[i]=='\t'){
-			print('        ');
-		}
-		if(buffer[i]=='\b'){
-			deleteChar();
-		}
-		else{
-			printChar(buffer[i])
-		}
-	}
-}
+#include <stdint.h>
+
+//void screenWriter(uint64_t buffer, uint64_t count, uint64_t upordown){
+//	for (int i = 0; i < count; i++){
+//		if(buffer[i]=='\n'){
+//			newLine(upordown);
+//		}
+//		if(buffer[i]=='\t'){
+//			print('        ');
+//		}
+//		if(buffer[i]=='\b'){
+//			deleteChar();
+//		}
+//		else{
+//			printChar(buffer[i])
+//		}
+//	}
+//}
