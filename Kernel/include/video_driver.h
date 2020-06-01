aa328629c1439f2e47714a13d393a5b98248cdbf
GastonDonikian <gdonikian@itba.edu.ar>
2020-06-01 01:24:48 -0300

    printManager con bugs

--- Kernel/include/video_driver.h
+++ Kernel/include/video_driver.h
@@ -3,6 +3,7 @@
 void writePixel(int x, int y);
 void writeScreen(int x, int y);
 void printCharByPixel(int x, int y,unsigned char ascii);
+char isItWriten(int x, int y);
 /*void printLineByPixel(int x, int y, char * string);
 void printChar(char ascii);
 void printLine(char * string);
--- Kernel/printManager.c
+++ Kernel/printManager.c
@@ -1,16 +1,132 @@
-void screenWriter(uint64_t buffer, uint64_t count, uint64_t upordown){
+#include <stdint.h>
+#include "video_driver.h"
+#include "printManager.h"
+#define WIDTH 1024 
+#define HEIGHT 768
+#define MULT 2
+#define CHAR_SIZE MULT*8
+#define LINE HEIGHT/(CHAR_SIZE)
+#define CHARACTERS WIDTH/(CHAR_SIZE*SCREENS)
+#define SCREENS 2
+#define SCREEN_WIDTH WIDTH/SCREENS
+
+unsigned char posScreen[SCREENS] = {0};
+unsigned char lineScreen[SCREENS] = {1,1}; 
+unsigned char select = 0;
+unsigned char upDown = 1;
+
+void screenWriter(char * buffer, uint64_t count, uint64_t upordown){
+	upDown = upordown;
 	for (int i = 0; i < count; i++){
 		if(buffer[i]=='\n'){
-			newLine(upordown);
+			newLine(); //CAMBIAR ACA?
 		}
 		if(buffer[i]=='\t'){
-			print('        ');
+			print("		");
 		}
 		if(buffer[i]=='\b'){
 			deleteChar();
 		}
 		else{
-			printChar(buffer[i])
+			printChar(buffer[i]);
 		}
 	}
 }
+
+/*void selector(unsigned char screen){ //CAMBIA EL SELECT Y INICIALIZA LA PRIMER LINEA
+	select = screen;
+	initializeScreen(select);
+}*/
+
+void selector() {
+	if(select)
+		select--;
+	else
+		select++;
+}
+
+void printChar(unsigned char ascii){ //UPDEATEADO UPDOWN
+	if(upDown == 1) {
+		lineScreen[select] = LINE -1;
+	}	
+	if(posScreen[select] >= CHARACTERS) {
+		newLine();
+	}
+	printCharByPixel(posScreen[select]*CHAR_SIZE + SCREEN_WIDTH*select,lineScreen[select]*CHAR_SIZE,ascii);
+	posScreen[select]++;
+}
+
+void print(char * string) { //UPDATEADO UPDOWN
+	int i = 0;
+	while(string[i] != 0) {
+		printChar(string[i++]);
+	}
+}
+
+void printLine(char * string) {
+	print(string);
+	newLine();
+}
+
+void cleanLine(unsigned char line){ //BORRA LA LINEA DE LA PANTALLA SELECCIONADA
+	for(int i = 0; i < SCREEN_WIDTH;i++) {
+		for(int j = 0; j < CHAR_SIZE; j++) {
+			writeScreen(i + SCREEN_WIDTH*select,j + line*CHAR_SIZE);
+		}
+	}
+}
+
+void newLine() { //UPDATEADO UPDOWN
+	if(upDown == 1) {
+		for(int i = 2; i < LINE - 1;i++) {
+			cleanLine(i-1);
+			copyLine(i,i-1); //COPIO LA LINEA I EN LA LINEA I -1
+		}
+		cleanLine(LINE-1);
+	}
+	else {
+		if(lineScreen[select] == LINE-1) {
+			clearScreen(select);
+			lineScreen[select] = 0;
+		}
+		lineScreen[select]++;
+		posScreen[select] = 0;
+	}
+}
+
+void copyLine(unsigned char source, unsigned char destiny) {
+	cleanLine(destiny);
+	for(int i = 0; i < SCREEN_WIDTH;i++) 
+		for(int j = 0; j < CHAR_SIZE; j++) 
+			if(isItWriten(i,j+source*CHAR_SIZE))
+				writePixel(i,j+destiny*CHAR_SIZE);
+		
+}
+
+void delete() {
+	for(int i = 0; i < CHAR_SIZE;i++)
+			for(int j = 0; j < CHAR_SIZE;j++)
+				writeScreen(i + posScreen[select],j + LINE - 1);
+	posScreen[select]--;
+}
+
+void deleteChar() {
+	if(upDown) {
+		delete();
+	}
+	else{
+		if(posScreen[select] == 0) {
+			lineScreen[select]--;
+			posScreen[select] = CHARACTERS;
+		}
+		delete();
+	}
+}
+//void selector(unsigned char screen);
+//void initializeScreen();
+//void changeScreen(void); //SE HACE EN SCREENMANAGER
+//void printLine(char * string);
+//void clearScreen();
+//void newLine(void);
+// void printCharAt(unsigned char ascii, unsigned char line);
+//void print(char * string);
